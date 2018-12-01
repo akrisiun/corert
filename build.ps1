118b33d066d3a7377efda9fff1c01ab809dd15ec
AndriusK <akrisiun@gmail.com>
2018-12-01 19:56:48 +0200

    testing hacks + EventSource

--- build.ps1
+++ build.ps1
@@ -4,3 +4,22 @@ dotnet build   src\System.Private.CoreLib\src\System.Private.CoreLib.csproj --no
 
 dotnet restore src\ILCompiler.Compiler\src\ILCompiler.Compiler.csproj
 dotnet build   src\ILCompiler.Compiler\src\ILCompiler.Compiler.csproj  --no-restore -o $PWD/bin
+
+dotnet restore src\System.Private.Threading\src\System.Private.Threading.csproj
+dotnet build   src\System.Private.Threading\src\System.Private.Threading.csproj       --no-restore -o $PWD/bin
+
+dotnet restore src\System.Private.Interop\src\System.Private.Interop.Mono.csproj
+dotnet build   src\System.Private.Interop\src\System.Private.Interop.Mono.csproj      --no-restore -o $PWD/bin
+
+dotnet restore src\System.Private.Interop\src\System.Private.Interop.CoreCLR.csproj
+dotnet build   src\System.Private.Interop\src\System.Private.Interop.CoreCLR.csproj   --no-restore -o $PWD/bin
+
+dotnet restore src\System.Private.DeveloperExperience.Console\src\System.Private.DeveloperExperience.Console.cs...
+dotnet build src\System.Private.DeveloperExperience.Console\src\System.Private.DeveloperExperience.Console.csproj
+dotnet publish src\System.Private.DeveloperExperience.Console\src\System.Private.DeveloperExperience.Console.csproj `
+  -o $PWD/bin  -r win-x64 -c Debug
+
+# dotnet restore src\System.Private.TypeLoader\src\System.Private.TypeLoader.csproj
+# dotnet build   src\System.Private.TypeLoader\src\System.Private.TypeLoader.csproj  -o $PWD/bin
+
+dotnet run src\System.Private.DeveloperExperience.Console\src\System.Private.DeveloperExperience.Console.csproj
--- samples/HelloWorld/Program.cs
+++ samples/HelloWorld/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+#if SUPPORT_JIT
+extern alias System_Private_CoreLib;
+using TextWriter = System_Private_CoreLib::System.IO.TextWriter;
+#endif
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main() // string[] args
         {
-            Console.WriteLine("Hello World!");
+            //  String str = "Hello World!";
+            // Object obj = str;
+            // Console.WriteLine(str);
+            return 0;
         }
     }
 }
+
+/*
+namespace ILCompiler
+{
+    using System.IO;
+    using TextWriter = System_Private_CoreLib::System.IO.TextWriter;
+    // Poor man's logger. We can do better than this.
+
+    public class Logger
+    {
+        // public static Logger Null = new Logger(TextWriter.Null, false);
+
+        public TextWriter Writer { get; }
+
+        public bool IsVerbose { get; }
+
+        public Logger(TextWriter writer, bool isVerbose)
+        {
+            Writer = writer;
+            IsVerbose = isVerbose;
+        }
+    }
+}
+
+*/
